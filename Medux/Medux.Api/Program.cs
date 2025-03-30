734d30ee78c53fffc8321eb2df57fe1e3a62dfcf
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-03-30 20:51:10 -0300

    feat: configuracoes de conexao

--- Medux/Medux.Api/Program.cs
+++ Medux/Medux.Api/Program.cs
@@ -1,6 +1,23 @@
+using DotNetEnv;
+using Medux.Application;
+using Medux.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+Env.Load();
+
+string username = Environment.GetEnvironmentVariable("MONGO_INITDB_ROOT_USERNAME") ?? "default_user";
+string password = Environment.GetEnvironmentVariable("MONGO_INITDB_ROOT_PASSWORD") ?? "default_pass";
+string database = Environment.GetEnvironmentVariable("MONGO_INITDB_DATABASE") ?? "default_db";
+string host = "mongodb";
+string port = Environment.GetEnvironmentVariable("MONGO_PORT") ?? "27017";
+
+string connectionString = $"mongodb://admin:senha@mongodb:27017/?authSource=admin";
+Console.WriteLine($"MongoDB Connection String: {connectionString}");
+
+builder.Services.ConfigurePersistance(connectionString, database);
+builder.Services.ConfigureApplication();
 
 builder.Services.AddControllers();
 
---
+++ Medux/Medux.Infra/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Medux.Domain.Models.Usuarios;
+using Medux.Infra.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+
+namespace Medux.Infra
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistance(this IServiceCollection services,string stringConexao,string nomeBanco) 
+        {
+            services.AddSingleton<IMongoClient>(ServiceProvider => new MongoClient(stringConexao));
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>(ServiceProvider =>
+                new UsuarioRepository(ServiceProvider.GetRequiredService<IMongoClient>(), nomeBanco, "Usuarios")
+            );
+        }
+    }
+}
